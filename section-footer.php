cb0dfb7d97590dae99ce81c2471d1c54ace20e8a
Andreu García <anduwet2@gmail.com>
2019-12-26 23:28:20 +0100

    facebook fix

--- section-footer.php
+++ section-footer.php
@@ -10,7 +10,7 @@
         </span>
         <ul class="list-inline social-buttons">
           <li><a href=<?=$twitter?>><i class="fab fa-twitter"></i></a></li>
-          <li><a href=<?=$facebook?>><i class="fab fa-facebook"></i></a></li>
+          <li><a href=<?=$facebook?>><i class="fab fa-facebook-f"></i></a></li>
           <li><a href=<?=$linkedin?>><i class="fab fa-linkedin-in"></i></a></li>
         </ul>
       </div>
